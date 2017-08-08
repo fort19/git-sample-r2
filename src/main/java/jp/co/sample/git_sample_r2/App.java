a2c0b1a8add74b90a5fc924859fc28ac00a3371f
fort19 <kingstar.yokohama@gmail.com>
2017-08-08 13:57:57 +0900

    新規プロジェクトを作成しました。

---
+++ src/main/java/jp/co/sample/git_sample_r2/App.java
@@ -0,0 +1,13 @@
+package jp.co.sample.git_sample_r2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
