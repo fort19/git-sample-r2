eea5246f6c15700e20e0a70a0aaa778d131dc4be
fort19 <kingstar.yokohama@gmail.com>
2017-08-08 15:27:05 +0900

    コミット

--- src/main/java/jp/co/sample/git_sample_r2/Test.java
+++ src/main/java/jp/co/sample/git_sample_r2/Test.java
@@ -1,8 +1,14 @@
 package jp.co.sample.git_sample_r2;
 
 public class Test {
+	private Integer id;
 	private String name;
 
+	public Test(Integer id, String name){
+		this.id = id;
+		this.name = name;
+	}
+	
 	public String getName() {
 		return name;
 	}
@@ -10,6 +16,16 @@ public class Test {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
 	
 	
 }
